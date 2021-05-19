ec6b28b9e2e5644c81f840de255aaa88c17a6903
Deep Bhatt <34996348+bhatt-deep@users.noreply.github.com>
2021-05-19 18:11:36 -0400

    Resolving conflict by in App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,11 @@ import { Row, Col } from 'react-bootstrap'
 import Menu from './components/menu'
 import NFTForm from './components/form'
 
-import Image from './components/image'
-import Testart from './components/testartCertificate'
-import Artist from './components/artist'
-import Hash from './components/hash'
-
 function App() {
   return (
     <div className="App">
       <Menu />
       <Row className="landing">
-        {/* <Col>
-          <Image />
-          <br></br>
-          <Hash />
-        </Col>
-        <Col>
-          <Testart />
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Artist />
-        </Col> */}
         <Col>
           <NFTForm />
         </Col>
