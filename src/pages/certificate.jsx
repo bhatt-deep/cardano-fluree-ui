2fa21e031196bcd5633069a93969a46020f33aa2
Deep Bhatt <34996348+bhatt-deep@users.noreply.github.com>
2021-05-22 20:34:13 -0400

    Implemented Fetching of metadata from api

--- src/pages/certificate.jsx
+++ src/pages/certificate.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import { Row, Col } from 'react-bootstrap'
 import Menu from '../components/menu'
+import axios from 'axios'
 
 import Image from '../components/image'
 import Testart from '../components/testartCertificate'
@@ -10,18 +11,29 @@ import Artist from '../components/artist'
 import Hash from '../components/hash'
 
 export default function Certificate() {
+  const [responseData, setResponseData] = useState('');
+  const apiUrl = 'http://localhost:3001/';
+  useEffect(() => {
+    axios.get(apiUrl).then((response) => {
+      const artdata= response.data.result;
+      setResponseData(artdata)
+      console.log(artdata)
+     // console.log(responseData)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
-    return (
+  return (
         <div class="App">
-         
           <Row>
             <Col>
               <Image />
               <br></br>
-              <Hash />
+              <Hash hashData={responseData} />
             </Col>
             <Col>
-              <Testart />
+              <Testart testArtData={responseData}/>
               <br></br>
               <br></br>
               <br></br>
@@ -30,7 +42,7 @@ export default function Certificate() {
               <br></br>
               <br></br>
     
-              <Artist />
+              <Artist  artistData={responseData} />
             </Col>
             </Row>
         </div>
