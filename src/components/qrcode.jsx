3c67d227e8ce5052c043f5521c8c3c3013a0c5a4
Deep Bhatt <34996348+bhatt-deep@users.noreply.github.com>
2021-05-24 23:06:29 -0400

    Fixed QRcode

--- src/components/qrcode.jsx
+++ src/components/qrcode.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import QRCode from 'qrcode.react'
+import QRCode from 'react-qr-code'
 
 const styles = {
     root: {
@@ -15,10 +15,10 @@ const styles = {
 
 
   export default function QrCode(props) {
-
+    const hash = props.data.txhHash
     return (
         <div style={styles.qrcode}>
-            <QRCode style={{ width: 100,height: 120, paddingLeft : 10}} value={JSON.stringify(props.data)} />
+            <QRCode style={{ width: 100,height: 100, paddingLeft : 10}} value={"http://localhost:3000/explore/"+hash} />
         </div>
     )
 }
